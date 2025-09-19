3615ecd6b101dd55741a1b5220154d184fc55457
boudi425 <bodybrkt@gmail.com>
2025-09-20 00:52:10 +0300

    New Update, New Lesson Added

---
+++ Beginner Level (1lvl - 10lvl)/level 3/main.js
@@ -0,0 +1,12 @@
+// الان نأتي إلى المتغيرات في جافا سكريبت
+// هناك ثلاث طرق لتعريف المتغيرات في جافا سكريبت
+// 1. var
+// 2. let
+// 3. const
+var UserName = "John Doe";
+
+let username = "John Doe";
+
+const userName = "John Doe";
+
+console.log(username);
